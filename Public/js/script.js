06ef3a10de18d7e8ecee76eaee4d42305c23d695
Kalel <kalelacez@gmail.com>
2025-06-25 18:45:08 -0300

    novos objetivos

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,4 +1,5 @@
-// refinar funcionalidade e concertar bugs
+// adicionar quantidade ao lado dos alimentos
+// atualizar html - interface bonita e mais eficiente e adicionar title "alimentos"
 let refeicao = document.getElementById("refeicao");
 let calorias = document.getElementById("calorias");
 let quantCal = 0;
@@ -6,7 +7,7 @@ let quantCal = 0;
 function adicionarRef(){
     let quantidade = document.getElementById("quantidade").value;
     if(quantidade <= 0){
-        alert("ERRO - quantidade invalida")
+        alert("ERRO - quantidade invalida");
     } else {
     let opcaoEscolhida = document.getElementById("opcao");
     let selectedOption = opcaoEscolhida.options[opcaoEscolhida.selectedIndex];
